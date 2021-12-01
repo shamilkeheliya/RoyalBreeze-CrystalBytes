e86f9d696783a45fa5eb09cd27134351976927b4
Hashini99 <bhhrkumari@students.nsbm.ac.lk>
2021-12-02 00:53:53 +0530

    Update connection.java

--- RoyalBreez/src/java/model/connection.java
+++ RoyalBreez/src/java/model/connection.java
@@ -3,6 +3,8 @@ package model;
 import static java.lang.System.out;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class connection {
@@ -37,5 +39,43 @@ public class connection {
         }        
         return conn;
     }
+    /*public  boolean checkUser(String email,String pass) 
+                {
+                  boolean st =false;
+                  try {
+
+                      
+                      PreparedStatement ps = createConnection().prepareStatement("select * from login where email=? and pass=?");
+                      ps.setString(1, email);
+                      ps.setString(2, pass);
+                      ResultSet rs =ps.executeQuery();
+                      st = rs.next();
+
+                  }
+                    catch(Exception e) {
+                        e.printStackTrace();
+                    }
+        
+                     return st;   
+    
+               }*/
+    public  boolean regUser(String name, String email,String pass) throws ClassNotFoundException, SQLException 
+                {
+                    PreparedStatement ps = createConnection().prepareStatement("query");
+                    ps.setString(1, firstname);
+                    ps.setString(2, lastname);
+                    ps.setString(3, email);
+                    ps.setString(4, password);
+                    ps.setString(5, phone);
+                    ps.setString(6, country);
+                    int i = ps.executeUpdate();
+
+                    if(i > 0) 
+                        return true;
+                      else
+                        return false;
+                    
+                }
+
     
 }
