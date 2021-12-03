9f8d116fa9d810206a7bb6cc8ee68a0b7dedf845
shamilkeheliya <shamilkeheliya@gmail.com>
2021-12-03 21:04:23 +0530

    Update connection.java

--- RoyalBreez/src/java/model/connection.java
+++ RoyalBreez/src/java/model/connection.java
@@ -1,5 +1,4 @@
 package model;
-
 import static java.lang.System.out;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -39,43 +38,25 @@ public class connection {
         }        
         return conn;
     }
+    
+    
     public  boolean checkUser(String email,String pass) 
-                {
-                  boolean st =false;
-                  try {
-
-                      
-                      PreparedStatement ps = createConnection().prepareStatement("select * from customers where email=? and password=?");
-                      ps.setString(1, email);
-                      ps.setString(2, pass);
-                      ResultSet rs =ps.executeQuery();
-                      st = rs.next();
-                  }
-                    catch(Exception e) {
-                        e.printStackTrace();
-                    }
-        
-                     return st;   
-    
-               }
-    
-    public  boolean regUser(String firstname, String lastname,String email, String password,String phone,String country) throws ClassNotFoundException, SQLException 
-                {
-                    PreparedStatement ps = createConnection().prepareStatement("query");
-                    ps.setString(1, firstname);
-                    ps.setString(2, lastname);
-                    ps.setString(3, email);
-                    ps.setString(4, password);
-                    ps.setString(5, phone);
-                    ps.setString(6, country);
-                    int i = ps.executeUpdate();
-
-                    if(i > 0) 
-                        return true;
-                      else
-                        return false;
-                    
-                }
+    {
+        boolean st =false;
+        try
+        { 
+            PreparedStatement ps = createConnection().prepareStatement("select * from customers where email=? and password=?");
+            ps.setString(1, email);
+            ps.setString(2, pass);
+            ResultSet rs =ps.executeQuery();
+            st = rs.next();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return st;   
+    }
     
     public boolean sqlCommand(String query) throws ClassNotFoundException, SQLException
     {
